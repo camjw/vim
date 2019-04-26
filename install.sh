0c0a65adf186c20b6ef98dca331d4063343121ea
camjw <camjw119@gmail.com>
2019-04-26 22:28:44 +0100

    Added some stuff, removed some stuff

--- install.sh
+++ install.sh
@@ -1,6 +1,23 @@
-#! /usr/bin/bash
+#! /bin/bash
 
 ln -sf ~/.vim/.vimrc ~/.vimrc
 git submodule update --init
 git submodule update --recursive --remote
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+echo ${machine}
+
+if [ ${machine} = Mac ]
+then
+	echo "Running on OSX, installing df font"
+	cp ~/.vim/fonts/df_font.ttf ~/Library/Fonts/
+fi
+
 echo "Vim configuration successfully installed"
